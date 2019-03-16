93c9dd8fd17edc63ca7d68c5b8106db3c8f30aa5
Byron <kilian.byron@gmail.com>
2019-03-16 13:17:27 +0200

    Scratching around to test numba performance

--- test.py
+++ test.py
@@ -1,6 +1,23 @@
+import numpy as np
 from numba import njit, jit
+from datetime import datetime
 
-@njit
-def loop(trials):
+@jit
+def loop1(trials):
+	for i in np.linspace(start=1,stop=trials,num=trials):
+		i*i
+
+def loop2(trials):
 	for i in range(trials):
-		i**2
+		i*i
+
+start_time1 = datetime.now()
+loop1(1000000)
+end_time1 = datetime.now()
+
+start_time2 = datetime.now()
+loop2(1000000)
+end_time2 = datetime.now()
+
+print('Execusion time Numba: ', end_time1 - start_time1)
+print('Execusion time Normal: ', end_time2 - start_time2)
